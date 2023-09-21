38b16a0f905e50c8a07e29cfa20fd88133e4984a
TulioMota <42577102+TulioMota@users.noreply.github.com>
2023-09-21 15:00:51 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://backendfoodexplorer-5pci.onrender.com/'
+  baseURL: 'https://backend-foodexplorer-08sk.onrender.com'
 });
